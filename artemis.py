76a85224cc97316f98809533fc5786f001eaacf7
gmvivekanandan <joker12345.rm@gmail.com>
2022-04-03 20:51:55 +0530

    initial commit

---
+++ artemis.py
@@ -0,0 +1,135 @@
+import argparse
+import os
+import sys
+import json
+
+from static import *
+
+# PATH
+base_path = os.getcwd()
+report_path = base_path+"/reports"
+
+# PARSER
+parser = argparse.ArgumentParser()
+parser.add_argument("--elf", help="toggle for ELF files", action="store_true")
+parser.add_argument("file", help="the file to analyse")
+args = parser.parse_args()
+
+# PARSE ARGUMENTS
+file_name = args.file
+is_elf = args.elf
+
+# CHECK IF FILE EXISTS
+is_exists = os.path.isfile(file_name)
+if(is_exists):
+    print("Checking for /reports directory...")
+    try:
+        os.mkdir(report_path)  # MAKE REPORTS DIRECTORY
+    except OSError as error:
+        print("Directory exists!\n")
+        pass
+else:
+    print("File does not exist in the current directory!")
+    sys.exit(1)
+
+# INSTANTIATE REPORT FILE
+if(is_elf):
+    report_file_name = file_name.split()[0]
+else:
+    report_file_name = file_name.split('.')[0]
+final_report = report_path+"/"+report_file_name+".txt"
+
+# OPEN REPORT FILE
+f = open(final_report, 'w')
+
+# GENERATE REPORT
+f.write("=====[STATIC ANALYSIS RESULTS]=====\n\n")
+print("=====[STATIC ANALYSIS RESULTS]=====\n")
+static = Static(file_name)
+
+file_type = static.filetype()
+print(f"FileType: {file_type}", end='')
+f.write(f"FileType: {file_type}")
+
+file_size = static.get_filesize()
+print(f"Size of the file: {file_size} bytes")
+f.write(f"Size of the file: {file_size} bytes\n")
+
+file_md5 = static.get_md5()
+print(f"md5 checksum: {file_md5}\n")
+f.write(f"md5 checksum: {file_md5}\n\n")
+
+print("+++++[VIRUSTOTAL RESULTS]+++++\n")
+f.write("+++++[VIRUSTOTAL RESULTS]+++++\n\n")
+virustotal_results = static.virus_total()
+if(virustotal_results == None):
+    f.write("No results found!\n\n")
+    print("No results found!\n\n")
+else:
+    sorted_results = virustotal_results["data"]["attributes"]
+    f.write(json.dumps(sorted_results, indent=2))
+    f.write("\n\n")
+    print("Check the report file for full Virustotal results\n")
+    # THREAT CATEGORY
+    category = sorted_results["popular_threat_classification"]["popular_threat_category"]
+    print(f"Popular categories: \n{json.dumps(category, indent=2)}\n")
+    # THREAT NAME
+    name = sorted_results["popular_threat_classification"]["popular_threat_name"]
+    print(f"Popular names: \n{json.dumps(name, indent=2)}\n")
+    # AV DATA
+    av_data = sorted_results["last_analysis_stats"]
+    print(f"Popular AV detection: \n{json.dumps(av_data, indent=2)}\n")
+    # POPULAR AV SOLUTION RESULTS
+    av_results = sorted_results["last_analysis_results"]
+    print(f"Popular AV results: \n{json.dumps(av_results, indent=2)}\n\n")
+
+print("+++++[YARA ANALYSIS]+++++\n")
+f.write("+++++[YARA ANALYSIS]+++++\n\n")
+yara_results = static.yara()
+if yara_results == []:
+    print("No results matching any YARA rules!\n")
+    print("Try changing the YARA rule in /config/capabilities.yara with other yara rules\nDO NOT CHANGE THE NAME OF THE FILE!\n")
+    f.write("No results matching any YARA rules!\n\n")
+    f.write("Try changing the YARA rule in /config/capabilities.yara with other yara rule file\nDO NOT CHANGE THE NAME OF THE FILE!\n\n")
+else:
+    print(yara_results)
+    f.write(str(yara_results))
+    f.write("\n\n")
+
+print("+++++[STRINGS ANALYSIS]+++++\n")
+f.write("+++++[STRINGS ANALYSIS]+++++\n\n")
+strings_results = static.strings()
+print(strings_results)
+f.write(strings_results)
+f.write("\n")
+
+if(is_elf):
+    print("+++++[ELF ANALYSIS]+++++\n")
+    f.write("+++++[ELF ANALYSIS]+++++\n\n")
+
+    file_header_results = static.file_header()
+    print(file_header_results)
+
+    program_header_results = static.program_header()
+    print(program_header_results)
+
+    section_header_results = static.section_header()
+    print(section_header_results)
+
+    symbol_results = static.symbols()
+    print(symbol_results)
+
+    print("Entire ELF analysis written to the report file\n")
+    read_elf_results = static.read_elf()
+    f.write(read_elf_results)
+    f.write("\n\n")
+
+print("+++++[DEPENDENCIES]+++++\n")
+f.write("+++++[DEPENDENCIES]+++++\n\n")
+dependencies_results = static.dependencies()
+print(dependencies_results)
+f.write(dependencies_results)
+f.write("\n\n")
+
+# CLOSE REPORT FILE
+f.close()
---
+++ static.py
@@ -0,0 +1,84 @@
+import subprocess
+import os
+import hashlib
+import requests
+import yara
+
+
+class Static:
+
+    def __init__(self, mal_file):
+        self.file = mal_file
+        self.md5 = ""
+
+    def filetype(self):
+        output = subprocess.check_output(["file", self.file], text=True)
+        return output
+
+    def get_filesize(self):
+        output = os.path.getsize(self.file)
+        return output
+
+    def get_md5(self):
+        with open(self.file, "rb") as f:
+            self.md5 = hashlib.md5(f.read()).hexdigest()
+            return self.md5
+
+    def virus_total(self):
+        url = f"https://www.virustotal.com/api/v3/files/{self.md5}"
+        headers = {
+            "Accept": "application/json",
+            # VIRUSTOTAL API KEY
+            "x-apikey": "ba454f7f9a46f013dcef1daf5346aa86d89465d36cbb9ecd963a7a3279e109aa"
+        }
+        response = requests.request("GET", url, headers=headers)
+        if(response.status_code == 404):
+            return(None)
+        else:
+            response_data = response.json()  # CONVERT JSON TO PYTHON DICT
+            return(response_data)
+
+    def yara(self):
+        rule = yara.compile('config/capabilities.yara')
+        matches = rule.match(self.file)
+        return matches
+
+    def strings(self):
+        strings = subprocess.check_output(["strings", self.file], text=True)
+        return strings
+
+    def file_header(self):
+        file_header = subprocess.check_output(
+            ["readelf", "-h", self.file], text=True)
+        return file_header
+
+    def program_header(self):
+        program_header = subprocess.check_output(
+            ["readelf", "-l", self.file], text=True)
+        return program_header
+
+    def section_header(self):
+        section_header = subprocess.check_output(
+            ["readelf", "-S", self.file], text=True)
+        return section_header
+
+    def symbols(self):
+        symbols = subprocess.check_output(
+            ["readelf", "-s", self.file], text=True)
+        return symbols
+
+    def read_elf(self):
+        read_elf = subprocess.check_output(
+            ["readelf", "-a", self.file], text=True)
+        return read_elf
+
+    def dependencies(self):
+        try:
+            dependencies1 = subprocess.Popen(
+                ["objdump", "-p", self.file], stdout=subprocess.PIPE)
+            dependencies2 = subprocess.check_output(
+                ["grep", "NEEDED"], stdin=dependencies1.stdout, text=True)
+            dependencies1.stdout.close()
+        except:
+            return "No results found\n"
+        return dependencies2
